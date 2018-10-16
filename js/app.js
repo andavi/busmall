3afdd2bcd8a59b9e17696beb4bf74a0f8f4b3536
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-10-16 12:12:08 -0700

    store allProducts in localStorage

--- js/app.js
+++ js/app.js
@@ -20,43 +20,50 @@ var Product = function(src, name) {
 };
 
 // create Product objects
-new Product('img/bag.jpg', 'Bag');
-new Product('img/banana.jpg', 'Banana');
-new Product('img/bathroom.jpg', 'Bathroom');
-new Product('img/boots.jpg', 'Boots');
-new Product('img/breakfast.jpg', 'Breakfast');
-new Product('img/bubblegum.jpg', 'Bubblegum');
-new Product('img/chair.jpg', 'Chair');
-new Product('img/cthulhu.jpg', 'Cthulhu');
-new Product('img/dog-duck.jpg', 'Dog Duck');
-new Product('img/dragon.jpg', 'Dragon');
-new Product('img/pen.jpg', 'Pen');
-new Product('img/pet-sweep.jpg', 'Pet Sweep');
-new Product('img/scissors.jpg', 'Scissors');
-new Product('img/shark.jpg', 'Shark');
-new Product('img/sweep.png', 'Sweep');
-new Product('img/tauntaun.jpg', 'Tauntaun');
-new Product('img/unicorn.jpg', 'Unicorn');
-new Product('img/usb.gif', 'USB');
-new Product('img/water-can.jpg', 'Water Can');
-new Product('img/wine-glass.jpg', 'Wine Glass');
+if (!localStorage.getItem('allProducts')){
+  new Product('img/bag.jpg', 'Bag');
+  new Product('img/banana.jpg', 'Banana');
+  new Product('img/bathroom.jpg', 'Bathroom');
+  new Product('img/boots.jpg', 'Boots');
+  new Product('img/breakfast.jpg', 'Breakfast');
+  new Product('img/bubblegum.jpg', 'Bubblegum');
+  new Product('img/chair.jpg', 'Chair');
+  new Product('img/cthulhu.jpg', 'Cthulhu');
+  new Product('img/dog-duck.jpg', 'Dog Duck');
+  new Product('img/dragon.jpg', 'Dragon');
+  new Product('img/pen.jpg', 'Pen');
+  new Product('img/pet-sweep.jpg', 'Pet Sweep');
+  new Product('img/scissors.jpg', 'Scissors');
+  new Product('img/shark.jpg', 'Shark');
+  new Product('img/sweep.png', 'Sweep');
+  new Product('img/tauntaun.jpg', 'Tauntaun');
+  new Product('img/unicorn.jpg', 'Unicorn');
+  new Product('img/usb.gif', 'USB');
+  new Product('img/water-can.jpg', 'Water Can');
+  new Product('img/wine-glass.jpg', 'Wine Glass');
+
+  localStorage.setItem('allProducts', JSON.stringify(allProducts));
+} else {
+  allProducts = JSON.parse(localStorage.getItem('allProducts'));
+}
 
 
-// prototype methods
-Product.prototype.render = function(node) {
+
+// static methods
+Product.render = function(node, product) {
   var img = node.children[0];
   var text = node.children[1];
-  img.src = this.src;
-  text.textContent = this.name;
+  img.src = product.src;
+  text.textContent = product.name;
 };
 
-Product.prototype.voted = function() {
-  this.votes++;
-  this.appeared++;
+Product.voted = function(product) {
+  product.votes++;
+  product.appeared++;
 };
 
-Product.prototype.notVoted = function() {
-  this.appeared++;
+Product.notVoted = function(product) {
+  product.appeared++;
 };
 
 // create available index (of allProducts) array
@@ -77,26 +84,26 @@ deleteIndex(1);
 deleteIndex(2);
 
 var renderAll = function() {
-  allProducts[leftIndex].render(leftDiv);
-  allProducts[middleIndex].render(middleDiv);
-  allProducts[rightIndex].render(rightDiv);
+  Product.render(leftDiv, allProducts[leftIndex]);
+  Product.render(middleDiv, allProducts[middleIndex]);
+  Product.render(rightDiv, allProducts[rightIndex]);
 };
 renderAll();
 
 // helper functions
 var updateVotes = function(id) {
   if (id === 'left') {
-    allProducts[leftIndex].voted();
-    allProducts[middleIndex].notVoted();
-    allProducts[rightIndex].notVoted();
+    Product.voted(allProducts[leftIndex]);
+    Product.notVoted(allProducts[middleIndex]);
+    Product.notVoted(allProducts[middleIndex]);
   } else if (id === 'middle') {
-    allProducts[leftIndex].notVoted();
-    allProducts[middleIndex].voted();
-    allProducts[rightIndex].notVoted();
+    Product.notVoted(allProducts[leftIndex]);
+    Product.voted(allProducts[middleIndex]);
+    Product.notVoted(allProducts[middleIndex]);
   } else {
-    allProducts[leftIndex].notVoted();
-    allProducts[middleIndex].notVoted();
-    allProducts[rightIndex].voted();
+    Product.notVoted(allProducts[leftIndex]);
+    Product.notVoted(allProducts[middleIndex]);
+    Product.voted(allProducts[middleIndex]);
   }
 };
 
@@ -144,6 +151,9 @@ var handleClick = function(event) {
     var h2 = document.createElement('h2');
     h2.textContent = 'Results';
     resultsList.appendChild(h2);
+
+    // update allProducts in localStorage after 25 votes
+    localStorage.setItem('allProducts', JSON.stringify(allProducts));
 
     // renderList();
     renderChart();
